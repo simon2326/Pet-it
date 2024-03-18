c25bcad423592982c09307dd446e15f0ae8aa2aa
Simon <simon.correa@upb.edu.co>
2024-03-18 17:00:54 -0500

    Home carrousel v2

--- dev/js/home.js
+++ dev/js/home.js
@@ -1,2 +1,14 @@
 const carru = document.querySelector('.carru');
 const punto = document.querySelectorAll('.punto');
+
+punto.forEach((puntito, i)=>{
+    punto[i].addEventListener('click', ()=>{
+        let posicion = i
+        //posicion 0 , transformX 0%
+        //posición 1, transformX -50%
+        let operacion = posicion*-50
+
+        carru.style.transform = `translateX(${operacion}%)`
+
+    })
+} )
